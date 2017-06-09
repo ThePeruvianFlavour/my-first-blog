0983d53c53379b1939e6ae586afb19bce296c15f
Saul <cmonfeo@gmail>
2017-06-09 13:45:51 +1000

    all done

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.http import JsonResponse
+from django.core import serializers
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -40,4 +42,11 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+def spa_post_list(request):
+    return render(request, 'blog/spa_post_list.html', {})
+
+def json_all_posts(request):
+    posts = Post.objects.order_by('published_date')
+    data = serializers.serialize('json', posts)
+    return JsonResponse(data, safe=False)
 
